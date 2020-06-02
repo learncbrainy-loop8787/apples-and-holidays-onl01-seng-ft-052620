bf2854ba94fc18b4fa5fc986bdfcf52c53f44e6d
korey ko <koreykendrick1@gmail.com>
2020-06-02 19:16:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,6 +55,10 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts "season.capotalize:"
+    holiday.each do |holiday, item|
+      
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
